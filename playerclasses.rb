78abf504436b27a2eab35e5ca872165cd5393214
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-25 13:13:03 -0500

    phase 0 done

---
+++ playerclasses.rb
@@ -0,0 +1,12 @@
+require "pry"
+
+class Player1
+
+  def initalize (name, age, gender)
+    @name
+    @age
+    @gender
+  end
+end
+
+binding.pry
--- w02-rps.rb
+++ w02-rps.rb
@@ -1,1 +1,25 @@
-start of rock paper scissors project
+#Title: Rock Paper Scissors Game
+#Description:  Rock beat scissors, scissors beats paper, paper beats rock, 
+#require "pry"
+
+
+choices = ["rock", "paper", "scissors"]
+puts "Choose rock, paper, or scissors"
+
+puts " player 1 enter in rock, paper, or scissors."
+player_one = gets.chomp.downcase
+
+puts "player 2 enter in rock, paper, or scissors"
+player_two = gets.chomp.downcase
+
+
+	if (player_one =="rock" && player_two =="paper") ||
+		(player_one =="scissors" && player_two =="rock") ||
+		(player_one =="paper" && player_two =="scissors")
+		puts "#{player_one} wins "
+	elsif (player_one =="rock" && player_two =="scissors") ||
+		(player_one =="scissors" && player_two =="paper" ) ||
+		(player_one =="paper" && player_two =="rock")
+		puts "#{player_two} wins"
+	end
+	
