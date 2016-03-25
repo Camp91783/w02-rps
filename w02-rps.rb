1863eed531e60a188c64dfb0d9750c60d6fdbd8a
Peter Camp <Pete.f.Camp@gmail.com>
2016-03-25 13:18:06 -0500

    phase 0 done for real

--- w02-rps.rb
+++ w02-rps.rb
@@ -3,7 +3,8 @@
 #require "pry"
 
 
-choices = ["rock", "paper", "scissors"]
+choices = ["rock", "paper", "sciss
+	ors"]
 puts "Choose rock, paper, or scissors"
 
 puts " player 1 enter in rock, paper, or scissors."
@@ -16,10 +17,10 @@ player_two = gets.chomp.downcase
 	if (player_one =="rock" && player_two =="paper") ||
 		(player_one =="scissors" && player_two =="rock") ||
 		(player_one =="paper" && player_two =="scissors")
-		puts "#{player_one} wins "
+		puts "player 1 wins with #{player_one} "
 	elsif (player_one =="rock" && player_two =="scissors") ||
 		(player_one =="scissors" && player_two =="paper" ) ||
 		(player_one =="paper" && player_two =="rock")
-		puts "#{player_two} wins"
+		puts "player 2 wins with #{player_two}"
 	end
 	
